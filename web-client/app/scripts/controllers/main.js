eebcc19454c09dcf8c19e9fa09742028ce63c0c2
Sergey Golovin <golovim@gmail.com>
2014-10-26 22:43:43 +0300

    image updating has been implemented

--- web-client/app/scripts/controllers/main.js
+++ web-client/app/scripts/controllers/main.js
@@ -2,21 +2,31 @@
 
 angular.module('webRemoteViewerApp')
     .controller('MainCtrl', function($scope) {
-    	function handler(image){
-    		var i = image;
-    	}
-
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                handler(this.response);
-                console.log(this.response, typeof this.response);
-                var img = document.getElementById('screen');
-                var url = window.URL || window.webkitURL;
-                img.src = url.createObjectURL(this.response);
-            }
+        function handler(image) {
+            var i = image;
         }
-        xhr.open('GET', 'http://localhost:8880/');
-        xhr.responseType = 'blob';
-        xhr.send();
+
+        (function(timeout) {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                var self = this;
+                if (self.readyState == 4 && self.status == 200) {
+                    handler(self.response);
+                    console.log(self.response, typeof self.response);
+                    var img = document.getElementById('screen');
+                    var url = window.URL || window.webkitURL;
+                    img.src = url.createObjectURL(self.response);
+                }
+            }
+
+            function getImage() {
+                xhr.open('GET', 'http://localhost:8880/');
+                xhr.responseType = 'blob';
+                xhr.send();
+
+                setTimeout(getImage, timeout);
+            }
+            getImage();
+        })(200)
+
     });
