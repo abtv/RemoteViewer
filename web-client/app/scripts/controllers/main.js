4c35680d87afa427d50f097e5778874ad1911276
Sergey Golovin <golovim@gmail.com>
2014-10-28 12:46:19 +0300

    some changes in image requests' loop

--- web-client/app/scripts/controllers/main.js
+++ web-client/app/scripts/controllers/main.js
@@ -4,26 +4,30 @@ angular.module('webRemoteViewerApp')
     .controller('MainCtrl', function($scope) {
 
         $scope.url = 'http://localhost:8880/';
-        $scope.speed = '24';
+        $scope.speed = '10';
+        $scope.timeout = 1000 / +$scope.speed;
 
-        (function(timeout) {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                var self = this;
-                if (self.readyState == 4 && self.status == 200) {
-                    var img = document.getElementById('screen');
-                    var url = window.URL || window.webkitURL;
-                    img.src = url.createObjectURL(self.response);
+        (function() {
+            function getImage() {
+                try {
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        var self = this;
+                        if (self.readyState == 4 && self.status == 200) {
+                            var img = document.getElementById('screen');
+                            var url = window.URL || window.webkitURL;
+                            img.baseURI = $scope.url;
+                            img.src = url.createObjectURL(self.response);
+                        }
+                    }
+                    xhr.open('GET', $scope.url, true);
+                    xhr.responseType = 'blob';
+                    xhr.send();
+                    setTimeout(getImage, $scope.timeout);
+                } catch (ex) {
+
                 }
             }
-
-            function getImage() {
-                xhr.open('GET', $scope.url);
-                xhr.responseType = 'blob';
-                xhr.send();
-
-                setTimeout(getImage, timeout);
-            }
             getImage();
-        })(1000 / +$scope.speed)
+        })()
     });
