bac31f8eb4405eacc44ca4e334fddadf1c5b2d68
abtv <andreybutov@mail.ru>
2014-11-20 00:15:10 +0300

    Timeouts were decreased

--- src/main/scala/application/Settings.scala
+++ src/main/scala/application/Settings.scala
@@ -4,6 +4,6 @@ object Settings {
   val imageFormat = "jpg"
   val multicastAddress  = "225.4.5.6"
   val multicastPort     = 44446
-  val screenShotTimeout = 1000
-  val serverSendTimeout = 1000
+  val screenShotTimeout = 500
+  val serverSendTimeout = 500
 }
